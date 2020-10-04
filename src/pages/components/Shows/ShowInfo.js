30dcd75053e3aec30ade1a27eadcc4a4fd7354ff
Sarvagya <sarvagyasingh@navialifecare.com>
2020-10-04 18:13:31 +0530

    showdata undefined issue

--- src/pages/components/Shows/ShowInfo.js
+++ src/pages/components/Shows/ShowInfo.js
@@ -20,14 +20,14 @@ export default function ShowInfo(props) {
             </div>
             <div className="title">
                 <h4>
-                    {showData.name}
+                    {showData && showData.name}
                 </h4>
                 <span>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="14" height="14"><path fill="none" d="M0 0h24v24H0z" /><path d="M12 18.26l-7.053 3.948 1.575-7.928L.587 8.792l8.027-.952L12 .5l3.386 7.34 8.027.952-5.935 5.488 1.575 7.928L12 18.26zm0-2.292l4.247 2.377-.949-4.773 3.573-3.305-4.833-.573L12 5.275l-2.038 4.42-4.833.572 3.573 3.305-.949 4.773L12 15.968z" /></svg>
-                    {showData.rating.average}
+                    {showData && showData.rating.average}
                 </span>
             </div>
-            {showData.genres.map(genre => (
+            {showData && showData.genres.map(genre => (
                 <div key={genre} className="show-genre">{genre}</div>
             ))}
         </div>
